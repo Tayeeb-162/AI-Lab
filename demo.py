494866c0728de05ec59bf56eda183d1c2b2134a5
Tayeeb-162 <mohammedtayeeb.cs24@bmsce.ac.in>
2026-09-22 16:08:06 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,59 @@
+#Name
+x="Mohandas"
+print("Name: ",x)
+#floats
+y=6.1
+print("Height: ",y)
+#Age integers
+z=20
+print("Age: ",z)
+#Strings
+message="Hello World"
+print("Message: ",message)
+#Booleans
+is_student=True
+print("Is Student: ",is_student)
+#lists
+numbers=[1,2,3,4,5]
+print("Numbers: ",numbers)
+#tuples
+coordinates=(10,20)
+print("Coordinates: ",coordinates)
+#Dictionaries
+person={"Name":"Alice","Age":30}
+print("Person: ",person)
+#arithematic operations
+sum=5+10
+print("Sum: ",sum)
+difference=10-3
+print("Difference: ",difference)
+product=3*12
+print("Product: ",product)
+quotient=12/3
+print("Quotient: ",quotient)
+remainder=15%4
+print("Remainder: ",remainder)
+power=2**4
+print("Power: ",power)
+#if-else conditions
+print("Conditions")
+x = 15 
+if x > 5: 
+	print("x is greater than 5") 
+elif x == 5: 
+	print("x is equal to 5") 
+else: 
+	print("x is less than 5")
+#loops
+print("Loops")
+number=[1,2,3,4,5]
+for i in number:
+    print(i*2)
+a=0
+while a<5:
+    print(a)
+    a=a+1
+#functions
+def fun():
+    print("Welcome to AI World")
+fun()    
